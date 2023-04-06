400a8af18e88770c04a0ae88c00acf8fdbfd6d19
Hannes <hannes.priebe@web.de>
2023-04-06 21:09:25 +0200

    tested some text with refined

--- ReFinED/refinedTest.py
+++ ReFinED/refinedTest.py
@@ -4,14 +4,14 @@ filename = "D:/Hannes/Dokumente/Dokumente/Uni/Bachelorarbeit/Kiefer-Scholz Colle
 refined = Refined.from_pretrained(model_name='wikipedia_model_with_numbers',
                                   entity_set="wikidata")
 
-# spans = refined.process_text("In Hamburg ist das weather schön")
+entities = refined.process_text(
+    "Trackmania is a video game developed by Nadeo. Van Gogh painted a picutre once. Time is of the essence here. A Century ago I saw you leave Hamburg.")
+print(entities)
 
-# print(spans)
-
-with open(filename, "r", encoding="utf-8") as file:
-    lines = [line.rstrip() for line in file]
-    for line in lines:
-        print("---------------------------------")
-        print(line)
-        entities = refined.process_text(line)
-        print(entities)
+# with open(filename, "r", encoding="utf-8") as file:
+#     lines = [line.rstrip() for line in file]
+#     for line in lines:
+#         print("---------------------------------")
+#         print(line)
+#         entities = refined.process_text(line)
+#         print(entities)
