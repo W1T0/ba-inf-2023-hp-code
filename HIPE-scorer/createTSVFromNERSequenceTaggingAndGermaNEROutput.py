9c0fdac005c9af23d00af0b6fa497dac244477c9
Hannes <hannes.priebe@web.de>
2023-06-08 17:44:21 +0200

    changed filenames of tsv and fixed some errors

--- HIPE-scorer/createTSVFromNERSequenceTaggingAndGermaNEROutput.py
+++ HIPE-scorer/createTSVFromNERSequenceTaggingAndGermaNEROutput.py
@@ -1,10 +1,44 @@
 # the path to the file to read (usually a NER output file)
-# path = "./NER-german/germaNER/germaNER-output.txt"
-path = "./NER-german/sequence_tagging/sequence_tagging-output2.txt"
+path = "./NER-german/germaNER/germaNER-output.txt"
+#  path = "./NER-german/sequence_tagging/sequence_tagging-output2.txt"
 
 # the output path
-outputPath = "./HIPE-scorer/output-sequence_tagging/"
-# outputPath = "./HIPE-scorer/output-germaNER/"
+# outputPath = "./HIPE-scorer/output-tsv-sequence_tagging/"
+outputPath = "./HIPE-scorer/output-tsv-germaNER/"
+
+outputFilenames = [
+    "L9xx12xx-TL2068_1b_pdf",
+    "L9xx0527-TL2027_1a_pdf",
+    "L9xxxxxx-TL0234_1b_pdf",
+    "L9xxxxxx-TL2026_1b_pdf",
+    "L9xxxxxx-TL2055_1a_pdf",
+    "L9xxxxxx-TL2082_1a_pdf",
+    "L9xxxxxx-TL2112_1b_pdf",
+    "L921xx17-TL0283_2b_pdf",
+    "L92309xx-TL1016_2b_pdf",
+    "L9110213-TL0015_1a_pdf",
+    "L9120323-TL0033_1a_pdf",
+    "L9120903-TL0046_1a_pdf",
+    "L9130421-TL0063_1b_pdf",
+    "L9140528-TL0092_1b_pdf",
+    "L9150629-TL0111_1a_pdf",
+    "L9151117-TL0116_1b_pdf",
+    "L9170131-TL0136_2a_pdf",
+    "L9190421-TL0157_1a_pdf",
+    "L9191208-TL0169_2a_pdf",
+    "L9200320-TL0187_1b_pdf",
+    "L9200422-TL0001_1a_pdf",
+    "L9200626-TL0201_2a_pdf",
+    "L9200910-TL0220_1b_pdf",
+    "L9201110-TL0230_1a_pdf",
+    "L9210103-TL0249_1a_pdf",
+    "L9210529-TL0272_1b_pdf",
+    "L9220904-TL0307_1b_pdf",
+    "L9221214-TL0322_1a_pdf",
+    "L9260206-TL1039_3b_pdf",
+    "L9300925-TL0222_1a_pdf",
+]
+
 
 # opens the file and reads all lines
 file = open(path, "r", encoding="utf-8")
@@ -29,13 +63,14 @@ lastCounter = 0
 #         # print("new previous line: " + previousLine)
 # print(len(lines))
 
+filename = ""
+
 # write every word
 for line in lines:
     if line.startswith("#"):
+        filename = outputFilenames[counter]
         counter += 1
-        print("[INFO] " + str(counter) + " Files written")
-
-    filename = "" + str(counter)
+        # print("[INFO] " + str(counter) + " Files written")
 
     lineSplit = line.split()
 
