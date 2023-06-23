99e889d0aa7aeca308216bba0c00e29b98806d95
Hannes <hannes.priebe@web.de>
2023-06-23 15:44:48 +0200

    commented out macro because of missing values in tsv files

--- HIPE-scorer/HIPE-results/calculateOverallResults.py
+++ HIPE-scorer/HIPE-results/calculateOverallResults.py
@@ -5,12 +5,14 @@ import numpy as np
 directories = [
     # "./HIPE-scorer/HIPE-results/HIPE-output-test/"
     "./HIPE-scorer/HIPE-results/output-hipe-flair-ner-german/",
-    # "./HIPE-scorer/HIPE-results/output-hipe-germaNER/",
-    # "./HIPE-scorer/HIPE-results/output-hipe-sequence_tagging/",
+    "./HIPE-scorer/HIPE-results/output-hipe-germaNER/",
+    "./HIPE-scorer/HIPE-results/output-hipe-sequence_tagging/",
 ]
 
 # iterate over directories
 for directory in directories:
+    print(directory)
+
     # Micro: true positive, false positive and false negative overall arrays
     # ALL LOC OTH ORG PER ALL LOC OTH ORG PER
     tpOverall = np.zeros((10))
@@ -62,9 +64,9 @@ for directory in directories:
             fnTemp = []
 
             # Macro: create  temporary lists for precision, recall, f1
-            pTemp = []
-            rTemp = []
-            f1Temp = []
+            # pTemp = []
+            # rTemp = []
+            # f1Temp = []
 
             # Micro: get values out of first 8 lines and append them to lists. First the category (ALL, LOC, etc.) and then the tp, fp or fn value
             for line in firstNMicroMinus1Lines:
@@ -75,20 +77,20 @@ for directory in directories:
 
             # Macro: get values out of p, r and f1 columns and append them to lists
             # TODO account for missing values
-            for line in firstNMacroMinus1Lines:
-                lineSplit = line.split()
-                print(lineSplit)
-                pTemp.append([lineSplit[2], float(lineSplit[3])])
-                rTemp.append([lineSplit[2], float(lineSplit[4])])
-                f1Temp.append([lineSplit[2], float(lineSplit[5])])
+            # for line in firstNMacroMinus1Lines:
+            #     lineSplit = line.split()
+            #     print(lineSplit)
+            #     pTemp.append([lineSplit[2], float(lineSplit[3])])
+            #     rTemp.append([lineSplit[2], float(lineSplit[4])])
+            #     f1Temp.append([lineSplit[2], float(lineSplit[5])])
 
-            print(pTemp)
-            print(rTemp)
-            print(f1Temp)
+            # print(pTemp)
+            # print(rTemp)
+            # print(f1Temp)
 
             # Micro: stores lists in list
             listsMicroTemp = [tpTemp, fpTemp, fnTemp]
-            listsMacroTemp = [pTemp, rTemp, f1Temp]
+            # listsMacroTemp = [pTemp, rTemp, f1Temp]
 
             # Micro: iterate over lists in lists
             for listTemp, listOverall in list(zip(listsMicroTemp, listsMicroOverall)):
@@ -119,38 +121,38 @@ for directory in directories:
                     elif entry[0] == "PER":
                         listOverall[9] += entry[1]
 
-            # Macro: iterate over lists in lists
-            for i in range(4):
-                for listTemp, listOverall in list(
-                    zip(listsMacroTemp, listsMacroOverall)
-                ):
-                    quarterOfList = int(len(listTemp) / 4)
-                    # sum the first quarter of the entries in the temporary list
-                    for entry in listTemp[quarterOfList * i : quarterOfList * (i + 1)]:
-                        if entry[0] == "ALL":
-                            listOverall[(5 * i) + 0] += entry[1]
-                        elif entry[0] == "LOC":
-                            listOverall[(5 * i) + 1] += entry[1]
-                        elif entry[0] == "OTH":
-                            listOverall[(5 * i) + 2] += entry[1]
-                        elif entry[0] == "ORG":
-                            listOverall[(5 * i) + 3] += entry[1]
-                        elif entry[0] == "PER":
-                            listOverall[(5 * i) + 4] += entry[1]
+            # # Macro: iterate over lists in lists
+            # for i in range(4):
+            #     for listTemp, listOverall in list(
+            #         zip(listsMacroTemp, listsMacroOverall)
+            #     ):
+            #         quarterOfList = int(len(listTemp) / 4)
+            #         # sum the first quarter of the entries in the temporary list
+            #         for entry in listTemp[quarterOfList * i : quarterOfList * (i + 1)]:
+            #             if entry[0] == "ALL":
+            #                 listOverall[(5 * i) + 0] += entry[1]
+            #             # elif entry[0] == "LOC":
+            #             #     listOverall[(5 * i) + 1] += entry[1]
+            #             # elif entry[0] == "OTH":
+            #             #     listOverall[(5 * i) + 2] += entry[1]
+            #             # elif entry[0] == "ORG":
+            #             #     listOverall[(5 * i) + 3] += entry[1]
+            #             # elif entry[0] == "PER":
+            #             #     listOverall[(5 * i) + 4] += entry[1]
 
     # print(tpOverall)
     # print(fpOverall)
     # print(fnOverall)
 
-    print("fileCount: " + str(fileCount))
-    # Macro: calculate precision, recall, f1
-    macroPrecision = pOverall / fileCount
-    macroRecall = rOverall / fileCount
-    microf1 = f1Overall / fileCount
+    # print("fileCount: " + str(fileCount))
+    # # Macro: calculate precision, recall, f1
+    # macroPrecision = pOverall / fileCount
+    # macroRecall = rOverall / fileCount
+    # microf1 = f1Overall / fileCount
 
-    print("Macro Precision: " + str(macroPrecision))
-    print("Macro Recall: " + str(macroRecall))
-    print("Macro F1: " + str(microf1))
+    # print("Macro Precision: " + str(macroPrecision))
+    # print("Macro Recall: " + str(macroRecall))
+    # print("Macro F1: " + str(microf1))
 
     # Micro: calculate precision, recall, f1 (add small number to prevent divison of zero)
     microPrecision = tpOverall / (tpOverall + fpOverall + 0.000000000000001)
@@ -160,6 +162,9 @@ for directory in directories:
         / (microPrecision + microRecall + 0.000000000000001)
     )
 
+    print("TP: " + str(tpOverall))
+    print("FP: " + str(fpOverall))
+    print("FN: " + str(fnOverall))
     print("Micro Precision: " + str(microPrecision))
     print("Micro Recall: " + str(microRecall))
     print("Micro F1: " + str(microf1))
