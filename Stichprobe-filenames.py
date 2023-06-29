a761ed5a18f461d52b2eaa0e94a7f288d1644ff3
Hannes <hannes.priebe@web.de>
2023-06-29 12:46:30 +0200

    sorted filenames

--- Stichprobe-filenames.py
+++ Stichprobe-filenames.py
@@ -30,3 +30,40 @@ outputFilenames = [
     "L9260206-TL1039_3b_pdf",
     "L9300925-TL0222_1a_pdf",
 ]
+
+outputFilenames.sort()
+print(outputFilenames)
+
+
+outputFilenamesSortes = [
+    "L9110213-TL0015_1a_pdf",
+    "L9120323-TL0033_1a_pdf",
+    "L9120903-TL0046_1a_pdf",
+    "L9130421-TL0063_1b_pdf",
+    "L9140528-TL0092_1b_pdf",
+    "L9150629-TL0111_1a_pdf",
+    "L9151117-TL0116_1b_pdf",
+    "L9170131-TL0136_2a_pdf",
+    "L9190421-TL0157_1a_pdf",
+    "L9191208-TL0169_2a_pdf",
+    "L9200320-TL0187_1b_pdf",
+    "L9200422-TL0001_1a_pdf",
+    "L9200626-TL0201_2a_pdf",
+    "L9200910-TL0220_1b_pdf",
+    "L9201110-TL0230_1a_pdf",
+    "L9210103-TL0249_1a_pdf",
+    "L9210529-TL0272_1b_pdf",
+    "L921xx17-TL0283_2b_pdf",
+    "L9220904-TL0307_1b_pdf",
+    "L9221214-TL0322_1a_pdf",
+    "L92309xx-TL1016_2b_pdf",
+    "L9260206-TL1039_3b_pdf",
+    "L9300925-TL0222_1a_pdf",
+    "L9xx0527-TL2027_1a_pdf",
+    "L9xx12xx-TL2068_1b_pdf",
+    "L9xxxxxx-TL0234_1b_pdf",
+    "L9xxxxxx-TL2026_1b_pdf",
+    "L9xxxxxx-TL2055_1a_pdf",
+    "L9xxxxxx-TL2082_1a_pdf",
+    "L9xxxxxx-TL2112_1b_pdf",
+]
