12b30ed1fb02cb154d487370920a208a3ec62857
Hannes <hannes.priebe@web.de>
2023-07-18 21:36:13 +0200

    calculateOverallResults output is now csv

--- A_calculateOverallResults.py
+++ A_calculateOverallResults.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import csv
 
 # the directories where the hipe output is stored
 directories = [
@@ -158,14 +159,51 @@ for directory in directories:
     # Micro: calculate precision, recall, f1 (add small number to prevent divison of zero)
     microPrecision = tpOverall / (tpOverall + fpOverall + 0.000000000000001)
     microRecall = tpOverall / (tpOverall + fnOverall + 0.000000000000001)
-    microf1 = 2 * (
-        (microPrecision * microRecall)
-        / (microPrecision + microRecall + 0.000000000000001)
+    microf1 = 2 * ((microPrecision * microRecall) / (microPrecision + microRecall + 0.000000000000001))
+
+    version = 5
+
+    # open file to write into
+    writeToFile = open(
+        "./HIPE-results/output-hipe-" + str(version) + "-overall-results.csv",
+        "a",
+        encoding="utf-8",
+        newline="",
     )
 
-    print("TP: " + str(tpOverall))
-    print("FP: " + str(fpOverall))
-    print("FN: " + str(fnOverall))
-    print("Micro Precision: " + str(microPrecision))
-    print("Micro Recall: " + str(microRecall))
-    print("Micro F1: " + str(microf1))
+    # create csv writer
+    CSVWriter = csv.writer(writeToFile)
+
+    # write directory name
+    CSVWriter.writerow([directory])
+
+    # write header
+    CSVWriter.writerow("Micro-Precision Micro-Recall Micro-F1 TP FP FN".split(" "))
+
+    # write data
+    for i in range(10):
+        CSVWriter.writerow(
+            (
+                str(microPrecision[i])
+                + " "
+                + str(microRecall[i])
+                + " "
+                + str(microf1[i])
+                + " "
+                + str(tpOverall[i])
+                + " "
+                + str(fpOverall[i])
+                + " "
+                + str(fnOverall[i])
+            ).split(" ")
+        )
+
+    # write new line
+    CSVWriter.writerow("\n")
+
+    # print("TP: " + str(tpOverall))
+    # print("FP: " + str(fpOverall))
+    # print("FN: " + str(fnOverall))
+    # print("Micro Precision: " + str(microPrecision))
+    # print("Micro Recall: " + str(microRecall))
+    # print("Micro F1: " + str(microf1))
